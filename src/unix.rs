3368348a42d6c36dbf8cb8e9ced0750ff282e7ba
Antti Rae <anttirae@gmail.com>
2023-10-23 21:04:22 +0300

    jotain koodia, emt en käy ulkona

--- src/unix.rs
+++ src/unix.rs
@@ -1,25 +1,28 @@
 use std::fs;
+use std::fs::File;
+use std::io::Read;
 use regex::Regex;
 
 pub fn get_info() -> Vec<String>{
     let mut info = Vec::new();
 
-    info.push(get_username());
-    info.push(get_desktop());
-    info.push(get_hostname());
-    info.push(get_uptime());
-    info.push(get_memory());
-    info.push(get_os_release());
-    info.push(get_shell());
+    info.push(format!("Username: {}", get_username()));
+    info.push(format!("Distro: {}", get_os_release()));
+    info.push(format!("Desktop: {}", get_desktop()));
+    info.push(format!("Hostname: {}", get_hostname()));
+    info.push(format!("Uptime: {}", get_uptime()));
+    info.push(format!("Memory: {}", get_memory()));
+    info.push(format!("Shell: {}", get_shell()));
 
     info
 }
 
 fn get_hostname() -> String {
-    let mut hostname = String::new();
-    hostname.push_str("hostname");
+    let file_path = "/proc/sys/kernel/hostname";
 
-    hostname
+    let contents = fs::read_to_string(file_path)
+        .unwrap_or_else(|_| String::new());
+    contents.trim_end_matches('\n').to_string()
 }
 
 fn get_username() -> String {
@@ -41,10 +44,35 @@ pub fn get_os_release() -> String {
     distro_id.to_string()
 }
 
-fn get_uptime() -> String {
-    let mut uptime = String::new();
-    uptime.push_str("uptime");
+fn format_uptime(seconds: f64) -> String {
+    let minutes = (seconds / 60.0).round();
+    let hours = (minutes / 60.0).round();
+    let days = (hours / 24.0).round();
 
+    format!("{} days, {} hours, {} minutes", days, hours % 24.0, minutes % 60.0)
+}
+
+fn get_uptime() -> String {
+    let file_path = "/proc/uptime";
+    let mut uptime: String = String::new();
+
+    match File::open(file_path) {
+        Ok(mut file) => {
+            file.read_to_string(&mut uptime).unwrap();
+            let uptime_seconds = uptime.split(".").next().unwrap();
+            uptime = String::from(uptime_seconds);
+
+        }
+        Err(e) => {
+            uptime.push_str("0");
+        }
+    }
+    let uptime_as_f64: f64 = uptime.parse().expect("Failed to convert uptime to f64");
+    let uptime: String = String::from("1234.56"); // your string
+
+    let uptime_as_f64: f64 = uptime.parse().expect("Failed to convert uptime to f64");
+
+    println!("{}", uptime_as_f64);
     uptime
 }
 
